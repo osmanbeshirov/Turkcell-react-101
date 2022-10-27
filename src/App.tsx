4bb986bb1a0cd0eadf6acb38102eee533fb6a20a
osmanbeshirov <osman.beshirov@gmail.com>
2022-10-28 01:53:31 +0300

    conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
+import Login from './components/Login';
+
+const fullname: string = 'Osman Bashirov'
+const isLoggedIn: boolean = false;
 
 
 function App() {
   return (
     <div className="App">
+      {/* first letter always is must be Uppercase */}
+      {/* {isLoggedIn ? fullname : 'Sign in'} */}
+      {isLoggedIn && fullname || <Login/>}
       <Header />
     </div>
   );
